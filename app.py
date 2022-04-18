9b3d3200afee0b6d3ef82a185b29a811f57c0325
michaelpinatton <73756983+MichaelPinatton@users.noreply.github.com>
2022-04-18 18:40:18 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -102,8 +102,8 @@ with st.expander("ℹ️  -- More info", expanded=False):
 	    """)
 with st.expander("🐶  -- Examples", expanded=False):
 
-    st.image("/ex/ex-pie-chart.png")
-    st.image("/ex/ex-excel.png")
+    st.image("https://github.com/MichaelPinatton/seo-directories-analyzer/blob/main/ex/ex-pie-chart.png")
+    st.image("https://github.com/MichaelPinatton/seo-directories-analyzer/blob/main/ex/ex-excel.png")
 
     st.markdown("")
 
